a9786ced39bcbc9a96e322e68a551f843817001d
mamammel <mamammel@iu.edu>
2020-05-25 17:35:29 -0500

    initial files added

---
+++ frontend/bottle_app.py
@@ -0,0 +1,22 @@
+from bottle import default_app, route, post, get, template, run, static_file
+
+@route("/static/css/<filename>")
+def css(filename):
+    return static_file(filename, root="./static/css/")
+
+
+@route("/static/js/<filename>")
+def js(filename):
+    return static_file(filename, root="./static/js/")
+
+@get("/static/images/<filename>")
+def image(filename):
+    return static_file(filename, root="./static/images/")
+
+@route("/")
+def home():
+    return template("index.html")
+
+application = default_app()
+
+run(host="localhost", port=8080, debug=True)
