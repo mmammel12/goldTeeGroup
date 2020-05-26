b4134d65dbef7a854ca2bf5028eb9b861b558661
mamammel <mamammel@iu.edu>
2020-05-26 14:59:48 -0500

    moved nav, footer. renamed file

---
+++ bottle_app/HTML.py
@@ -0,0 +1,31 @@
+HEAD = '''<head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="/static/css/goldTee.css">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+            <link rel="icon" type="image/x-icon" href="/static/images/favicon.ico"/>
+            <title>Gold Tee Group</title>
+        </head>'''
+
+NAV = '''<nav class="navbar navbar-default navbar-fixed-top shadow wbycNav">
+            <div class="container pl-0">
+                <div class="navbar-header">
+                    <a class="d-none d-md-inline pl-3 py-3 logo-full" href="https://www.wbyc.com/" tabindex="0">
+                        <img src="/static/images/logo_full.png" srcset="/static/images/logo_full-2x.png 2x" alt="White Bear Yacht Club">
+                    </a>
+                    <a class="d-md-none logo-mobile" href="https://www.wbyc.com/" tabindex="0">
+                        <img src="/static/images/flag_accent.png" srcset="/static/images/flag_accent-2x.png 2x" alt="White Bear Yacht Club">
+                    </a>
+                    <h2 class="navbar-brand text-light d-none d-md-inline py-0 pl-5 ml-5">Gold Tee Group</h2>
+                </div>
+            </div>
+        </nav>'''
+
+FOOTER = '''<footer class="footer">
+            <div class="container-fluid">
+                <span class="text-light">Copyright ©2020 by <a class="text-light font-weight-bold" href="https://martymammel.com" target="_blank" title="Marty Mammel">Marty Mammel</a> With Permission From <a class="text-light font-weight-bold" href="https://www.wbyc.com/" target="_blank" title="White Bear Yacht Club">White Bear Yacht Club</a>. All Rights Reserved.</span>
+            </div>
+        </footer>'''
