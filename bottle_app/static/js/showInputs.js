6c718bce17bac6221c8c913b4a5df7b640e310c8
mamammel <mamammel@iu.edu>
2020-05-27 10:24:14 -0500

    update creating inputs

--- bottle_app/static/js/showInputs.js
+++ bottle_app/static/js/showInputs.js
@@ -1,59 +1,66 @@
-let numInputs = $("#numPlayersSelect").val();
+$(function() {
+    $("#numPlayersSelect").change(function () {
+        let numPlayers = $("#numPlayersSelect").val();
+        let numInputs = $("#formInputs").length;
+        if (numInputs < numPlayers) {
+            numInputs = numPlayers;
+            let handicap, front9Inputs, back9Inputs;
 
-$("#numPlayersSelect").change(function () {
-    let numPlayers = $("#numPlayersSelect").val();
-    if (numInputs < numPlayers) {
-        numInputs = numPlayers;
-
-        for (playerNum = 1; playerNum < numPlayers; playerNum++) {
-            let handicap = `<div class="row">
+            for (playerNum = 1; playerNum < numPlayers; playerNum++) {
+                handicap = `<div class="row">
     <div class="ml-3 mw-25">
         <label for="p${playerNum}handicap">Player Handicap</label>
         <input type="number" class="form-control" id="p${playerNum}handicap" name="p${playerNum}handicap" max=100 min=-20>
     </div>
 </div>`;
-            for (holeNum = 1; holeNum <= 9; holeNum++) {
-                let id = `p${playerNum}h${holeNum}Input`;
-                let name = `p${playerNum}h${holeNum}`;
-                let front9Inputs = `<div class="row">
-    <div class="col">
-        <label class="d-none d-md-inline" for=${id}>Hole ${holeNum}</label>
-        <label class="d-md-none" for=${id}>${holeNum}</label>
-        <input type="number" class="form-control" id=${id} name=${name} min=0>
-    </div>
+                for (holeNum = 1; holeNum <= 9; holeNum++) {
+                    let id = `p${playerNum}h${holeNum}Input`;
+                    let name = `p${playerNum}h${holeNum}`;
+                    front9Inputs += `<div class="col">
+    <label class="d-none d-md-inline" for=${id}>Hole ${holeNum}</label>
+    <label class="d-md-none" for=${id}>${holeNum}</label>
+    <input type="number" class="form-control" id=${id} name=${name} min=0>
+</div>
+`;
+                }
+
+                for (holeNum = 10; holeNum <= 18; holeNum++) {
+                    let id = `p${playerNum}h${holeNum}Input`;
+                    let name = `p${playerNum}h${holeNum}`;
+                    back9Inputs += `<div class="col">
+    <label class="d-none d-md-inline" for=${id}>Hole ${holeNum}</label>
+    <label class="d-md-none" for=${id}>${holeNum}</label>
+    <input type="number" class="form-control" id=${id} name=${name} min=0>
+</div>
+`;
+                }
+
+                let front9Div = `<div class="row">
+    ${front9Inputs}
 </div>`;
+                let back9Div = `<div class="row">
+    ${back9Inputs}
+</div>`;
+
+                let inputDiv = `<div id="player${playerNum}">
+        <div class="form-group border rounded p-3">
+            ${handicap}
+            ${front9Div}
+            ${back9Div}
+        </div>
+    </div>`;
+
+                $("#formInputs").append(inputDiv);
+            }
+        }
+        else if (numInputs > numPlayers) {
+            while (numInputs > numPlayers) {
+                $("#formInputs").remove(`#player${numInputs}`);
+                numInputs--;
             }
 
-            for (holeNum = 10; holeNum <= 18; holeNum++) {
-                let id = `p${playerNum}h${holeNum}Input`;
-                let name = `p${playerNum}h${holeNum}`;
-                let back9Inputs = `<div class="row">
-    <div class="col">
-        <label class="d-none d-md-inline" for=${id}>Hole ${holeNum}</label>
-        <label class="d-md-none" for=${id}>${holeNum}</label>
-        <input type="number" class="form-control" id=${id} name=${name} min=0>
-    </div>
-</div>`;
-            }
-
-            let inputDiv = `<div id="player${playerNum}">
-    <div class="form-group border rounded p-3">
-        ${handicap}
-        ${front9Inputs}
-        ${back9Inputs}
-    </div>
-</div>`;
-
-            $("#formInputs").append(inputDiv);
+            numInputs = numPlayers;
         }
-    }
-    else if (numInputs > numPlayers) {
-        while (numInputs > numPlayers) {
-            $("#formInputs").remove(`#player${numInputs}`);
-            numInputs--;
-        }
-
-        numInputs = numPlayers;
-    }
+    });
 });
 
