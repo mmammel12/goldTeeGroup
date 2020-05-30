a29984be9b2ca4ba59c15ad7fc9a7e0489531695
mamammel <mamammel@iu.edu>
2020-05-30 18:42:42 -0500

    add required on handicap input

--- bottle_app/static/js/showInputs.js
+++ bottle_app/static/js/showInputs.js
@@ -5,12 +5,14 @@ $(function() {
         if (numInputs < numPlayers) {
             for (i = numInputs; i < numPlayers; i++) {
                 $("#player" + i).show();
+                $("#p" + i + "handicap").prop("required", true);
             }
             numInputs = numPlayers;
         }
         else if (numInputs > numPlayers) {
             for (i = numInputs - 1; i >= numPlayers; i--) {
                 $("#player" + i).hide();
+                $("#p" + i + "handicap").prop("required", false);
             }
 
             numInputs = numPlayers;
